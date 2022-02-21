d05aa466d78f0e6409122dbac422bc65ee961629
DanielSteagall <daniel.steagall@kellogg.northwestern.edu>
2022-02-21 18:18:08 +0000

    v3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(params[place])
+        @post.save
+        redirect_to "/places/<%= place.id %>"
+    end
+
 end
