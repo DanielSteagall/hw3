9fc1a5a201d47770a04968c04447ca35360b14f3
DanielSteagall <daniel.steagall@kellogg.northwestern.edu>
2022-02-21 19:29:46 +0000

    v4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,13 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @post.place_id = params["place_id"]
     end
 
     def create
         @post = Post.new(params[place])
         @post.save
-        redirect_to "/places/<%= place.id %>"
+        redirect_to "/places"
     end
 
 end
