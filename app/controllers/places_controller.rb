55e7b949f148b6ed03170aef912b7069a294b075
DanielSteagall <daniel.steagall@kellogg.northwestern.edu>
2022-02-21 18:04:08 +0000

    v2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,3 +1,21 @@
 class PlacesController < ApplicationController
-    places = Place.All
+    
+    def index
+        @places = Place.All
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params[place])
+        @place.save
+        redirect_to "/places"
+    end
+
 end
